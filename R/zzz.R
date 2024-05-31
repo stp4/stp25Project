b144bcfa1199ba29f6fced88e8773130fcc5f5fe
stp4 <w.peter@statistik-peter.at>
2024-05-31 11:11:50 +0200

    Bankdaten

--- R/zzz.R
+++ R/zzz.R
@@ -1,3 +1,9 @@
+.onLoad <- function(libname, pkgname) {
+  packageStartupMessage("\nHi Wolfi!\n\nI wish you a good and successful working day.\n\n")
+}
+
+
+
 IBAN <- function() "AT89 2050 3033 0288 4626"
 phone <- function()  "+43 699 8153 0117"
 email <- function()  "w.peter@statistik-peter.at"
